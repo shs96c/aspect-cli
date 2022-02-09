8e65f30dfad767af9e4186f76dfd0788e1e2915b
Jason Bedard <jason@aspect.dev>
2022-02-09 15:41:11 -0800

    test: add PluginSystem.Configure() tests

--- pkg/plugin/system/system_test.go
+++ pkg/plugin/system/system_test.go
@@ -13,12 +13,14 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
+	goplugin "github.com/hashicorp/go-plugin"
 	. "github.com/onsi/gomega"
 	"github.com/spf13/cobra"
 
 	rootFlags "aspect.build/cli/pkg/aspect/root/flags"
 	"aspect.build/cli/pkg/aspecterrors"
 	"aspect.build/cli/pkg/ioutils"
+	"aspect.build/cli/pkg/plugin/sdk/v1alpha2/plugin"
 	plugin_mock "aspect.build/cli/pkg/plugin/sdk/v1alpha2/plugin/mock"
 	system_mock "aspect.build/cli/pkg/plugin/system/mock"
 )
@@ -274,5 +276,153 @@ func TestPluginSystemInterceptors(t *testing.T) {
 
 		g.Expect(err).NotTo(BeNil())
 		g.Expect(err.(*aspecterrors.ExitError).ExitCode).To(Equal(1))
+	})
+}
+
+// Finder returning an error
+type errorFinder struct{}
+
+func (f *errorFinder) Find() (string, error) {
+	return "", fmt.Errorf("Find Fail")
+}
+
+// PluginParser returning an error
+type errorParser struct{}
+
+func (f *errorParser) Parse(path string) ([]AspectPlugin, error) {
+	return nil, fmt.Errorf("Parse Fail")
+}
+
+// PluginParser returning a preset array of plugins
+type inlinePluginParser struct {
+	plugins []AspectPlugin
+}
+
+// ClientFactory - pops plugins of the array when New() is invoked
+type mockClientFactory struct {
+	plugins []InternalPlugin
+}
+
+func (cf *mockClientFactory) New(config *goplugin.ClientConfig) (*InternalPlugin, error) {
+	plugin, plugins := &cf.plugins[0], append(cf.plugins[:0], cf.plugins[0+1:]...)
+	cf.plugins = plugins
+	return plugin, nil
+}
+
+// Empty test plugin
+type TestPlugin struct {
+	plugin.Base
+}
+
+func (f *inlinePluginParser) Parse(path string) ([]AspectPlugin, error) {
+	return f.plugins, nil
+}
+
+func TestConfigure(t *testing.T) {
+	t.Run("fails when Finder fails to find plugin config file", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+
+		var stdout strings.Builder
+		streams := ioutils.Streams{Stdout: &stdout, Stderr: &stdout}
+
+		ps := &pluginSystem{
+			finder: &errorFinder{},
+		}
+
+		err := ps.Configure(streams)
+
+		g.Expect(err).NotTo(BeNil())
+	})
+
+	t.Run("fails when Parser fails to parse plugin config file", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		var stdout strings.Builder
+		streams := ioutils.Streams{Stdout: &stdout, Stderr: &stdout}
+
+		finder := system_mock.NewMockFinder(ctrl)
+		finder.EXPECT().Find().Return("/foo/bar")
+
+		ps := &pluginSystem{
+			finder: finder,
+			parser: &errorParser{},
+		}
+
+		err := ps.Configure(streams)
+
+		g.Expect(err).NotTo(BeNil())
+	})
+
+	t.Run("works when 0 plugins are found in config file", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		var stdout strings.Builder
+		streams := ioutils.Streams{Stdout: &stdout, Stderr: &stdout}
+
+		finder := system_mock.NewMockFinder(ctrl)
+		finder.EXPECT().Find().Return("/foo/bar")
+
+		ps := &pluginSystem{
+			finder: finder,
+			parser: &inlinePluginParser{plugins: []AspectPlugin{}},
+		}
+
+		err := ps.Configure(streams)
+
+		g.Expect(err).To(BeNil())
+	})
+
+	t.Run("creates and persists each plugin", func(t *testing.T) {
+		g := NewGomegaWithT(t)
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		var stdout strings.Builder
+		streams := ioutils.Streams{Stdout: &stdout, Stderr: &stdout}
+
+		testPlugin := AspectPlugin{
+			Name:     "test plugin",
+			From:     "...",
+			LogLevel: "debug",
+		}
+		testPlugin2 := AspectPlugin{
+			Name:     "test plugin2",
+			From:     "...",
+			LogLevel: "debug",
+		}
+
+		finder := system_mock.NewMockFinder(ctrl)
+		finder.EXPECT().Find().Return("/foo/bar")
+
+		p1 := &TestPlugin{}
+		p2 := &TestPlugin{}
+
+		ps := &pluginSystem{
+			finder: finder,
+			parser: &inlinePluginParser{plugins: []AspectPlugin{testPlugin, testPlugin2}},
+			clientFactory: &mockClientFactory{
+				plugins: []InternalPlugin{
+					{
+						plugin: p1,
+						client: system_mock.NewMockClientProvider(ctrl),
+					},
+					{
+						plugin: p2,
+						client: system_mock.NewMockClientProvider(ctrl),
+					},
+				},
+			},
+			plugins: &PluginList{},
+		}
+
+		err := ps.Configure(streams)
+
+		g.Expect(err).To(BeNil())
+		g.Expect(ps.plugins.head.plugin.plugin).To(Equal(p1))
+		g.Expect(ps.plugins.tail.plugin.plugin).To(Equal(p2))
 	})
 }
